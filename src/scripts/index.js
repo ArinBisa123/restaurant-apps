d5ba963dc4bd1fb597fb3cdf2a0647fd0d665627
ArinBisa123 <leejindairy06@gmail.com>
2024-04-25 18:01:45 +0700

    make custom elements

---
+++ src/scripts/index.js
@@ -0,0 +1,38 @@
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "../scripts/restaurant-list.js"
+
+const drawer = document.querySelector("#nav__drawer");
+const navMenu = document.querySelector("#nav__menu");
+const restaurantListElement = document.querySelector('restaurant-list')
+console.log(restaurantListElement)
+drawer.addEventListener("click", (event) => {
+  if (navMenu.classList.contains("open")) {
+    navMenu.classList.remove("open");
+  } else {
+    navMenu.classList.add("open");
+  }
+  event.stopPropagation();
+  // console.log("yo");
+});
+navMenu.addEventListener("click", () => {
+  navMenu.classList.remove("open");
+});
+
+const getAllData= ()=>{
+  fetch('../../src/DATA.json')
+    .then(response=>{
+      if(!response.ok){
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      return response.json()
+    })
+    .then (responseJson=>{
+      console.log(responseJson)
+      restaurantListElement.restaurant= responseJson
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+}
+console.log(getAllData())
